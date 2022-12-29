df9fb7337090be9603c0a606bc8e3f81a51c1b23
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2022-12-29 12:40:28 +0600

    initial commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  // COnfig code goes here
+  apiKey: "AIzaSyAtqXp1VUZtPE27sN9-c2ftHEqttTzTZ_A",
+  authDomain: "aricnc-352d1.firebaseapp.com",
+  projectId: "aricnc-352d1",
+  storageBucket: "aricnc-352d1.appspot.com",
+  messagingSenderId: "384276574086",
+  appId: "1:384276574086:web:7306a572767108f759c607"
+}
+
+const app = initializeApp(firebaseConfig)
+
+
+export default app
