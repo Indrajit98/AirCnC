1878c903d3068897830424341cd925ae632eca4b
Indrajit Chandra Chanda <indrajitchandra98@gmail.com>
2023-07-05 00:15:43 +0600

    checkout from component add

--- src/Components/Form/CheckoutForm.js
+++ src/Components/Form/CheckoutForm.js
@@ -1,30 +1,117 @@
-import React, { useEffect, useState } from 'react';
-import { getPaymentIntent } from '../../api/bookings';
+import React, { useEffect, useState } from 'react'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+// import { saveBooking } from '../../api/auth'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { getPaymentIntent, saveBooking } from '../../api/bookings'
 
-const CheckoutForm = ({bookingData}) => {
-    const [clientSecret, setClientSecret] = useState('')
-    console.log(bookingData);
+const CheckoutForm = ({ bookingData }) => {
+  const navigate = useNavigate()
+  const stripe = useStripe()
+  const elements = useElements()
+  const [cardError, setCardError] = useState('')
+  const [processing, setProcessing] = useState(false)
+  const [transactionId, setTransactionId] = useState('')
+  const [clientSecret, setClientSecret] = useState('')
 
-    useEffect(() => {
-        getPaymentIntent(bookingData?.totalPrice).then(data => { 
-            console.log(data);
-            setClientSecret(data.clientSecret)
+  const { totalPrice, guestName, guestEmail } = bookingData
+  console.log(bookingData);
+  console.log(clientSecret)
+  useEffect(() => {
+    getPaymentIntent(totalPrice).then(data => {
+      if (data?.clientSecret) {
+        setClientSecret(data.clientSecret)
+      }
+    })
+  }, [totalPrice])
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    if (!stripe || !elements) {
+      return
+    }
+
+    const card = elements.getElement(CardElement)
+
+    if (card === null) {
+      return
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card,
+    })
+
+    setCardError(error?.message || '')
+    setProcessing(true)
+    // confirm card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: guestName,
+            email: guestEmail,
+          },
+        },
+      })
+
+    if (intentError) {
+      setCardError(intentError?.message)
+      setProcessing(false)
+    } else {
+      setCardError('')
+      setTransactionId(paymentIntent.id)
+      console.log(paymentIntent)
+
+      //store payment on database
+      const data = {
+        transactionId: paymentIntent.id,
+        ...bookingData,
+      }
+      saveBooking(data)
+        .then(res => res.json())
+        .then(data => {
+          setProcessing(false)
+          console.log(data)
+          toast.success('Booking Successful!')
+          navigate('/dashboard/my-bookings')
         })
+        .catch(err => console.log(err))
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='my-10 max-w-lg'>
+        <CardElement
+          className='border p-4 rounded-md shadow-md'
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
+              },
+            },
+          }}
+        />
+        <button
+          className=' mt-4 bg-gradient-to-r from-emerald-500 to-lime-500 rounded-md text-white px-4 py-1'
+          type='submit'
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className='text-red-500'>{cardError}</p>}
+    </>
+  )
+}
 
-
-    },[bookingData?.totalPrice])
-
-    console.log(clientSecret);
-    console.log(bookingData.totalPrice);
-
-    return (
-        <div>
-            {
-
-            }
-            
-        </div>
-    );
-};
-
-export default CheckoutForm;
+export default CheckoutForm
